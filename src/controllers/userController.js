32696bf3d8fa019036b72c3e9e35aa9fc67cf980
Prashrijan <shresthaprashrijan@gmail.com>
2025-01-21 23:27:09 +1100

    login and access token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -53,38 +53,37 @@ const loginUser = async (req, res) => {
       message: "All fields are required.",
     });
   }
-  const user = await User.findOne({ email })
+  const user = await User.findOne({ email });
   if (!user) {
     return res.status(400).json({
       status: "failed",
       message: "User not found!",
-    })
+    });
   }
 
-  const isPasswordValid = await user.isPasswordCorrect(password)
+  const isPasswordValid = await user.isPasswordCorrect(password);
   if (!isPasswordValid) {
     return res.status(400).json({
       status: "failed",
       message: "Invalid Password!!",
-    })
+    });
   }
 
-  const accessToken = await user.generateAccessToken()
+  const accessToken = await user.generateAccessToken();
+
   if (!accessToken) {
     return res.status(400).json({
       status: "failed",
       message: "Unauthorised access!!!",
-    })
+    });
   }
-
 
   return res.status(201).json({
     status: "success",
     message: "Login Successful!!",
     data: user,
     accessToken,
-
-  })
+  });
 };
 
 export { registerUser, loginUser };
