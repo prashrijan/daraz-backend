543060e3f16b59ebb2bec60dcbced0403f03239f
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2025-01-21 21:53:19 +1100

    userschema and user model

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,1 +1,24 @@
-console.log("usermodel");
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    }
+}, { timestamps: true })
+
+export const User = new mongoose.model("User", userSchema)
